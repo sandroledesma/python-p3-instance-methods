56359857485d73c50ef07c8833a067bdca6d225c
Alessandro Ledesma <sandroledesma@gmail.com>
2024-06-05 13:07:55 -0400

    update files

--- lib/person.py
+++ lib/person.py
@@ -2,6 +2,14 @@
 
 class Person:
     # Class body goes here
+    def person(self):
+        pass
+
+    def talk(person):
+        print("Hello World!")
+
+    def walk(person):
+        print("The person is walking.")
 
     #Instance method definition
     pass
